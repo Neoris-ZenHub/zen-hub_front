a8db516f0a50d9b483f9caad5879ed0f33aed484
SantiagoCZB <s.carrizalesb04@gmail.com>
2024-04-24 20:54:22 -0600

    Marketplace completo

--- zen-hub_front/src/Marketplace/StoreItems.jsx
+++ zen-hub_front/src/Marketplace/StoreItems.jsx
@@ -15,11 +15,26 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function StoreItem({ key, name, image, color, cost }){
+
+    const handleBuy = async () => {
+        const response = await buyingSprites(key);
+
+        if (response.message === "Sprite bought successfully"){
+            alert("Compra Exitosa!");
+        }  else if (response.message === "Insufficient neorimas to complete purchase"){
+            alert("No tienes suficientes Neorimas");
+        } else if (response.message === "Sprite already bought"){
+            alert("Ya tienes este sprite");
+        } else {
+            alert("Ocurrió un error al intentar completar la compra")
+        }
+    }
+
     return (
         <Item style={{backgroundColor: color, width: '250px', height: '230px', borderRadius: '10px'}}>
             <img src={image} alt={name} style={{height: '70px', width: 'auto', marginTop: '30px'}}/>
             <h3>{name}</h3>
-            <Button variant="contained" onClick = {buyingSprites(key)} style={{backgroundColor: 'white', color: '#060606', width: '80%', height: '40px'}}>
+            <Button variant="contained" onClick = {handleBuy} style={{backgroundColor: 'white', color: '#060606', width: '80%', height: '40px'}}>
                 <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
                     <p>{cost}</p>
                     <img src={IconDark} alt="Icon Light" style={{width: '20px', height: 'auto', marginLeft: '10px'}}/>
